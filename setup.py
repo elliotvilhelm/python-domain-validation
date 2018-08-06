cbddbc53d2d523dc210c077d273355a656ef640c
ElliotVilhelm <elliot@pourmand.com>
2018-08-06 15:35:16 -0700

    release v1.0

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'whois-python',
+    packages = ['whois-python'],
+    version = '1.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'whois client and parser focused on creation date',
+    author = 'Elliot Pourmand',
+    author_email = 'elliot@pourmand.com',
+    url = 'https://github.com/ElliotVilhelm/python-whois',
+    download_url = 'https://github.com/ElliotVilhelm/python-whois/archive/v1.0.tar.gz',
+    keywords = ['whois', 'domain age'],
+    classifiers = [],
+)
