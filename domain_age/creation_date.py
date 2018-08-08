0497381bbd478ebe50ca66197856e53ea2c2f242
ElliotVilhelm <elliot@pourmand.com>
2018-08-07 22:54:27 -0700

    name

---
+++ domain_age/creation_date.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import re
+from whois_python.constants import SERVER_NOT_FOUND, CREATION_DATE_NOT_FOUND
+from whois_python.whois_client import query_whois
+from whois_python.parse_date import parse_date
+
+
+def get_domain_creation_date(domain):
+    try:
+        whois_response = query_whois(domain)
+        if whois_response == SERVER_NOT_FOUND:
+            return CREATION_DATE_NOT_FOUND
+        pattern = r"Creation Date|created|Created On|Creation date|Created|Domain Name Commencement Date|登録年月日|Registration Time|Registered on|registered"
+        res = re.search(pattern, whois_response)
+        if not res:
+            return CREATION_DATE_NOT_FOUND
+        creation_date_start = res.start()
+        creation_date = whois_response[creation_date_start:creation_date_start + 50]
+        date = re.search(r'\d', creation_date)
+        if not date:
+            return CREATION_DATE_NOT_FOUND
+        start_index = date.start()
+        date = creation_date[start_index:]
+        return parse_date(date)
+    except:
+        return CREATION_DATE_NOT_FOUND
+
---
+++ domain_age/parse_date.py
@@ -0,0 +1,43 @@
+import re
+from datetime import datetime
+from whois_python.constants import MONTHS
+
+
+def parse_date(date):
+    pattern = "Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec"
+    res = re.search(pattern, date, re.I)
+    if res:                              # 14 Apr 2018
+        day = int(date[0:2])
+        month = MONTHS[date[res.start():res.start() + 3]]
+        year = int(date[res.end() + 1: res.end() + 5])
+    elif '/' in date and date[2] == '/':  # DD/MM/YYY
+        day = int(date[0:2])
+        month = int(date[3:5])
+        year = int(date[6:10])
+    elif '/' in date:                     # YYYY/MM/DD
+        year = int(date[0:4])
+        month = int(date[5:7])
+        day = int(date[8:10])
+    elif '-' in date and date[2] == '-':  # DD-MM-YYYY
+        day = int(date[0:2])
+        month = int(date[3:5])
+        year = int(date[6:10])
+    elif '-' in date:                     # YYYY-MM-DD
+        year = int(date[0:4])
+        month = int(date[5:7])
+        day = int(date[8:10])
+    elif '.' in date and date[2] == '.':  # DD.MM.YYYY
+        day = int(date[0:2])
+        month = int(date[3:5])
+        year = int(date[6:10])
+    elif '.' in date:                     # YYYY.MM.DD
+        year = int(date[0:4])
+        month = int(date[5:7])
+        day = int(date[8:10])
+    else:                                 # YYYYMMDD
+        year = int(date[0:4])
+        month = int(date[4:6])
+        day = int(date[6:8])
+    date = datetime(year, month, day)
+    return date.date()
+
